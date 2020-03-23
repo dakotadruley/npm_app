2b5c30c4d0a4522a2d5091307d65937d78ed9bab
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-22 18:57:09 -0700

    added lsiteners

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const url = require('url');
 // pass the createServer function an anonymous function
 function start(route, handle) {
   function onRequest(req, res) {
+    const postData = '';
     const pathName = url.parse(req.url).pathName;
     console.log('Request for ' + pathName + ' recieved.');
     
-    route(handle, pathName, res);
+    req.setEncoding('utf8');
+
+    req.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('Recieved POST data chunk \'' +
+      postDataChunk + '\' .');
+    });
+
+    req.addListener('end', function() {
+      route(handle, pathName, res, postData);
+    });
   }
 
-  http.createServer(onRequest).listen(8000);
+  http.createServer(onRequest).listen(8888);
   console.log('Server has started.');
 
 }
