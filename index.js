85cb9545675fedda68e374ac2baad7c2a8394e55
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-22 18:14:25 -0700

    added request Hnadlers

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const server = require('./server.js');
 const router = require('./lib/router.js');
+const requestHandlers = require('./lib/requestHandlers.js');
+
+const handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
 
 server.start(router.route);
