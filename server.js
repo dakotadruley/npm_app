a8994d00aff78adc7afd22686621f1c85b86d89c
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-22 18:29:36 -0700

    added exec

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const http = require('http');
 const url = require('url');
 
 // pass the createServer function an anonymous function
-function start(route) {
+function start(route, handle) {
   function onRequest(req, res) {
     const pathName = url.parse(req.url).pathName;
     console.log('Request for ' + pathName + ' recieved.');
     
-    route(pathName);
+    route(handle, pathName);
     
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello World :)');
