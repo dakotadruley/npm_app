6fe8d0591460734f18cd249e1cc7aafb7a95476f
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-22 17:53:53 -0700

    index.js is now the main file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require('http');
 
 // pass the createServer function an anonymous function
-function onRequest(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello World :)');
-  res.end();
+function start() {
+  function onRequest(req, res) {
+    console.log('Request recieved.');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World :)');
+    res.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
